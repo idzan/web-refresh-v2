91f30b9c914092a8a4de1553961a4332e65a80d4
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-01-19 16:00:15 +0100

    Minor tweaks
    Filtered Favicons and Logos for web
    Sorted Analytics from Typed.js which is loaded only on homepage

--- js/app.js
+++ js/app.js
@@ -7,30 +7,3 @@ var typed = new Typed('.typing', {
     backDelay: 700,
     loop: true
 });
-// Analytics
-function getCookie(name) {
-    var b = document.cookie.match('(^|[^;]+)\\s*' + name + '\\s*=\\s*([^;]+)'); return b ? b.pop() : '';
-  }
-
-  function GoogleAnalytics() {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());  gtag('config', 'UA-47485892-14');
-  }
-
-  window.addEventListener("load", function () {
-    const cookieConsent = getCookie('cookieconsent_status');
-    if (cookieConsent === 'allow' || cookieConsent === '') {
-      GoogleAnalytics();        
-    }
-    window.cookieconsent.initialise({
-        "palette": { "popup": { "background": "#252e39" }, "button": { "background": "#14a7d0" } },
-        "theme": "edgeless",
-        "position": "bottom-right",
-        "type": "opt-in",
-        "content": { "message": "This website uses cookies to ensure you get the best experience on our website.", "dismiss": "Dismiss", "allow": "Allow",
-          "link": "Privacy Policy", "href": "https://idzan.eu/privacy-policy/" },
-
-          onStatusChange: function (status, chosenBefore) { location.reload(); }
-    })
-  });
