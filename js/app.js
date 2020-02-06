3cef5b9ad2a8beae4243a2af9ed2657269657ec9
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-06 01:05:50 +0100

    Included small js directly in page
    Sometimes i hate myself with doing doble work, but yeah because it is for a single page this is well, best solution
    to-do: find css only typing script if possible, if not use it like this

--- js/app.js
+++
@@ -1,9 +0,0 @@
-// Typed.js
-var typed = new Typed('.typing', {
-    strings: ['Freelance Web Designer', 'IT Technician', 'Web Administrator', 'IT Administrator', 'Photographer', 'Video Editor', 'Gamer and Nerd'],
-    typeSpeed: 30,
-    startDelay: 5,
-    backSpeed: 50,
-    backDelay: 700,
-    loop: true
-});
