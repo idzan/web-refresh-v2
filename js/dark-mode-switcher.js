c96740a14fc340efda68dba4f7dc5b7f054cedf7
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-15 00:52:01 +0100

    Android-Chrome Icon added
    Plus added it to dark-mode switcher
    to-do: add to all pages

--- js/dark-mode-switcher.js
+++ js/dark-mode-switcher.js
@@ -5,16 +5,19 @@ function handleDarkMode(e) {
 	var darkModeEnabled = e.matches;
 	var favicon = document.querySelector('link[type="image/x-icon"]'); // Old favicon
 	var appleTouchIcon = document.querySelector('link[rel="apple-touch-icon"]'); // iOS Icon
+	var androidIcon = document.querySelector('link[type="image/png"]'); // Android image
 
-	if (!favicon || !appleTouchIcon) {
+	if (!favicon || !appleTouchIcon || !androidIcon) {
 		return;
 	}
 	if (darkModeEnabled) {
 		favicon.href = '/favicon/icon-dark.ico';
 		appleTouchIcon.href = '/favicon/apple-touch-dark.png';
+		androidIcon.href = '/favicon/android-chrome-dark.png';
 	} else {
 		favicon.href = '/favicon/icon.ico';
 		appleTouchIcon.href = '/favicon/apple-touch-icon.png';
+		androidIcon.href = '/favicon/android-chrome.png';
 	}
 
 }
