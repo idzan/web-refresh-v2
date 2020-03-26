e82b190fb1379673a379f6237ce725a0669403cc
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-03-26 02:39:28 +0100

    Dark mode I hope so

--- js/dark-mode-switcher.js
+++ js/dark-mode-switcher.js
@@ -6,18 +6,21 @@ function handleDarkMode(e) {
 	var favicon = document.querySelector('link[type="image/x-icon"]'); // Old favicon
 	var appleTouchIcon = document.querySelector('link[rel="apple-touch-icon"]'); // iOS Icon
 	var androidIcon = document.querySelector('link[type="image/png"]'); // Android image
+	var gRecaptcha = document.querySelector('.g-recaptcha');
 
-	if (!favicon || !appleTouchIcon || !androidIcon) {
+	if (!favicon || !appleTouchIcon || !androidIcon || !gRecaptcha) {
 		return;
 	}
 	if (darkModeEnabled) {
 		favicon.href = '/favicon/icon-dark.ico';
 		appleTouchIcon.href = '/favicon/apple-touch-icon-dark.png';
 		androidIcon.href = '/favicon/android-chrome-dark.png';
+		gRecaptcha.dataset.theme = 'dark';
 	} else {
 		favicon.href = '/favicon/icon.ico';
 		appleTouchIcon.href = '/favicon/apple-touch-icon.png';
 		androidIcon.href = '/favicon/android-chrome.png';
+		gRecaptcha.dataset.theme = 'light';
 	}
 
 }
